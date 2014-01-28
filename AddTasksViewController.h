b8d366087f1a5d947af0baefd1efb423d0a30faf
jklotter <julie.klotter@gmail.com>
2014-01-28 14:04:06 -0600

    program just after class

---
+++ AddTasksViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTasksViewController.h
+//  Do-It2
+//
+//  Created by Julie Klotter on 1/28/14.
+//  Copyright (c) 2014 IOSClass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTasksViewController : UITableViewController
+
+@end
---
+++ Do-It2/TasksViewController.h
@@ -0,0 +1,13 @@
+//
+//  TasksViewController.h
+//  Do-It2
+//
+//  Created by Julie Klotter on 1/27/14.
+//  Copyright (c) 2014 IOSClass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TasksViewController : UITableViewController
+
+@end
